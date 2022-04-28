99ebd0da95a74591f0414a4b98ee76f8778f58b9
Lucas Souza <lucasp.sdev@gmail.com>
2022-04-27 21:38:53 -0300

    feat: finish Open Closed Principle

--- src/components/TodoList/TodoList.vue
+++ src/components/TodoList/TodoList.vue
@@ -1,25 +1,16 @@
 <template>
-  <div class="todo-list__container">
-    <div :key="todo.id" v-for="todo in todos" class="todo-list__tasks">
-      <span :class="{ 'todo-list__tasks-completed': todo.completed }">
-        {{ todo.title }}
-      </span>
+  <div class="todo-list">
+    <div class="todo-list__container">
+      <slot></slot>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { ITodos } from '@/helpers/interfaces/ITodos'
 
 export default defineComponent({
-  name: 'TodoList',
-  props: {
-    todos: {
-      type: Object as () => ITodos[],
-      required: true
-    }
-  }
+  name: 'TodoList'
 })
 </script>
 
--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -2,7 +2,14 @@
   <div class="home">
     <HomeHeader />
     <main>
-      <TodoList :todos="todos" />
+      <TodoList>
+        <TodoCard
+          v-for="todo in todos"
+          :key="todo.id"
+          :todoCompleted="todo.completed"
+          :todoTitle="todo.title"
+        />
+      </TodoList>
     </main>
   </div>
 </template>
@@ -11,12 +18,17 @@
 import { defineComponent } from 'vue'
 import { ITodos } from '@/helpers/interfaces/ITodos'
 import TodoList from '@/components/TodoList/TodoList.vue'
+import TodoCard from '@/components/TodoCard/TodoCard.vue'
 import HomeHeader from '@/components/HomeHeader/HomeHeader.vue'
 import Api from '@/api/api'
 
 export default defineComponent({
   name: 'HomeView',
-  components: { HomeHeader, TodoList },
+  components: {
+    HomeHeader,
+    TodoList,
+    TodoCard
+  },
   async mounted() {
     this.todos = await this.addTodo()
   },
