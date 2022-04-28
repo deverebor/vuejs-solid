1bdbbf20f3d134856baf97f5470cd29be011ba92
Lucas Souza <lucasp.sdev@gmail.com>
2022-04-28 20:07:22 -0300

    feat: finish Interface Segregation Principle

---
+++ src/components/TodoRow/TodoRow.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="todo-row">
+    <span>{{ todo.id }}</span>
+    <span :class="{ 'todo-row__completed': todo.completed }">
+      {{ todo.title }}
+    </span>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { ITodos } from '@/helpers/interfaces/ITodos'
+
+export default defineComponent({
+  name: 'TodoRow',
+  props: {
+    todo: {
+      type: Object as () => ITodos,
+      required: true
+    }
+  }
+})
+</script>
+<style src="./TodoRow.scss" lang="scss" scoped />
